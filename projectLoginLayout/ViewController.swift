c11adb434545a3e8da40c2c45a8a6be83ce59198
Daniel Washington <danielvertigo@hotmail.com>
2021-06-08 14:37:43 -0300

    Initial Commit

---
+++ projectLoginLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projectLoginLayout
+//
+//  Created by Daniel Washington Ignacio on 08/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
